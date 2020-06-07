b237abde3c7a43123f7388047a7cfe5c320d867d
Daniel Morais <daniel.morais@ifsp.edu.br>
2020-06-07 18:29:39 +0000

    Desativação do https (problemas com certificado no codespace) e inclusão do .gitignore

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,6 @@ namespace sampleaspnetapp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
